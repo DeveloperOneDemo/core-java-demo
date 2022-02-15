e041d86b5553b1c4252ea92a365e14e49e19387f
DeveloperOneDemo <developer2021one@gmail.com>
2022-02-15 13:10:37 -0500

    JDBC

--- jdbc-project/src/main/java/InsertJdbcDemo.java
+++ jdbc-project/src/main/java/InsertJdbcDemo.java
@@ -1,61 +1,66 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class SelectJdbcDemo {
+public class InsertJdbcDemo {
 
 	public static void main(String[] args) {
-	
 		
-		// step 1 - Load the driver into the memory
+		// step 1
 		try {
 			Class.forName("org.postgresql.Driver");
-			System.out.println("Driver Loaded....");
+			System.out.println("Driver Loaded...");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		// step 2
 		Connection conn = null;
 		try {
-			// step 2 - establish connection with DB
-			// specify ipaddress, port number, protocol, credentials
-			// connection url has protocol//ipaddress:portnumber/dbname
 			String connectionUrl = "jdbc:postgresql://localhost:5432/sample";
 			String userName = "postgres";
 			String password = "root";
 			
 			conn = DriverManager.getConnection(connectionUrl, userName, password);
+			
 			if(conn != null) {
 				System.out.println("Connection Established....");
 			}else{
 				System.out.println("Connection not Established....");
 			}
-			// step 3 -  create a statement and execute it.
-			// create the statement
+			
+			//step 3
 			Statement stmt = conn.createStatement();
-			// execute the statement
-			String query = "select * from student_details";
-			ResultSet rs = stmt.executeQuery(query); // for DQL
-			while(rs.next()) {
-				System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getInt(4));
-			}
+			int studId = 108;
+			String studName = "VBN";
+			String studCity = "St.Louis";
+			int studContact = 11111;
+			int studMark = 55;
+			
+			String query = "insert into student_details values("+studId+",'"+studName+"',"+studContact+","+studMark+",'"+studCity+"')";
+			System.out.println(query);
+			
+			int rows = stmt.executeUpdate(query); // for DML
+			
+			System.out.println(rows + " row(s) affected...");
 		} catch (SQLException e) {
-			// step 4 - handle the exceptions
+			//step 4
 			e.printStackTrace();
 		} finally {
 			try {
 				conn.close();
 			} catch (SQLException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		
 		
-		
-		
-		
 
+		
+		
+		
 	}
 
 }
---
+++ jdbc-project/src/main/java/InsertPreparedJdbcDemo.java
@@ -0,0 +1,75 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertPreparedJdbcDemo {
+
+	public static void main(String[] args) {
+		// step 1
+				try {
+					Class.forName("org.postgresql.Driver");
+					System.out.println("Driver Loaded...");
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+				
+				// step 2
+				Connection conn = null;
+				try {
+					String connectionUrl = "jdbc:postgresql://localhost:5432/sample";
+					String userName = "postgres";
+					String password = "root";
+					
+					conn = DriverManager.getConnection(connectionUrl, userName, password);
+					
+					if(conn != null) {
+						System.out.println("Connection Established....");
+					}else{
+						System.out.println("Connection not Established....");
+					}
+					
+					//step 3
+//					Statement stmt = conn.createStatement();
+//					int studId = 108;
+//					String studName = "VBN";
+//					String studCity = "St.Louis";
+//					int studContact = 11111;
+//					int studMark = 55;
+//					
+//					String query = "insert into student_details values("+studId+",'"+studName+"',"+studContact+","+studMark+",'"+studCity+"')";
+//					System.out.println(query);
+//					
+//					int rows = stmt.executeUpdate(query); // for DML
+					
+//					System.out.println(rows + " row(s) affected...");
+					
+					String query = "insert into student_details values(?,?,?,?,?)";					
+					PreparedStatement psmt = conn.prepareStatement(query);
+					
+					psmt.setInt(1, 112);
+					psmt.setString(2, "JKL");
+					psmt.setInt(3, 444444);
+					psmt.setInt(4, 45);
+					psmt.setString(5, "St.Louis");
+					
+					int row = psmt.executeUpdate();
+					
+					System.out.println(row + " row(s) affected...");
+					
+				} catch (SQLException e) {
+					//step 4
+					e.printStackTrace();
+				} finally {
+					try {
+						conn.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+	}
+
+}
