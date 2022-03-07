cca0e35e54239c433b0eac6791c15a741663e716
DeveloperOneDemo <92218513+DeveloperOneDemo@users.noreply.github.com>
2022-03-07 13:13:20 -0500

    Hibernate

---
+++ student-hibernate-demo/src/main/java/entity/StudentEntity.java
@@ -0,0 +1,76 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student_details")
+public class StudentEntity {
+
+	@Id
+	@Column(name="student_id")
+	private int studentId;
+	
+	@Column(name="student_name")
+	private String studentName;
+	
+	@Column(name="student_city")
+	private String studentCity;
+	
+	@Column(name="student_mark")
+	private int studentMark;
+
+	public StudentEntity() {
+		
+	}
+
+	public StudentEntity(int studentId, String studentName, String studentCity, int studentMark) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentCity = studentCity;
+		this.studentMark = studentMark;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentCity() {
+		return studentCity;
+	}
+
+	public void setStudentCity(String studentCity) {
+		this.studentCity = studentCity;
+	}
+
+	public int getStudentMark() {
+		return studentMark;
+	}
+
+	public void setStudentMark(int studentMark) {
+		this.studentMark = studentMark;
+	}
+
+	@Override
+	public String toString() {
+		return "StudentEntity [studentId=" + studentId + ", studentName=" + studentName + ", studentCity=" + studentCity
+				+ ", studentMark=" + studentMark + "]";
+	}
+	
+	
+}
